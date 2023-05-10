449ae887bb0af9c6887b0d62cea20cf44325592c
Enzo <45255580+sosipopu@users.noreply.github.com>
2023-05-10 16:35:31 +0200

    init

---
+++ src/main/java/com/pragmantic/managerapi/ManagerapiApplication.java
@@ -0,0 +1,16 @@
+package com.pragmantic.managerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+public class ManagerapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManagerapiApplication.class, args);
+    }
+
+
+
+}
---
+++ src/test/java/com/pragmantic/managerapi/ManagerapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pragmantic.managerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagerapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
